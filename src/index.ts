fc5bdd486f998c5cf4edfcd1603b7b50f3735dde
Agustí Moll <agusti@gmail.com>
2021-09-22 10:42:32 +0200

    Add prettier, lint and jest

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-import express from 'express'
+import app from './server'
 import { name, version } from '../package.json'
 
-const app = express()
 const port = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-    res.send("Hello!")
-})
-
-
 app.listen(port, () => {
-    console.log(`Start ${name} v.${version} in port ${port}`)
-    return
+  console.log(`Start ${name} v.${version} in port ${port}`)
+  return
 })
-
-
-
