48b3ed2f9f6293a7f69f3fdb90dbbfaf7a4a62ed
Agustí Moll <agusti@gmail.com>
2021-09-22 11:17:01 +0200

    Add helmet & cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
 
 const app = express()
 
+app.use(helmet())
+app.use(cors())
+
+app.use(express.json())
+
 app.get('/', (req, res) => {
-  res.send('Hello!')
+  res.send({ hello: 'world' })
 })
 
 app.get('/test', (req, res) => {
-  res.send('test')
+  res.send({ test: 'test' })
 })
 
 export default app
