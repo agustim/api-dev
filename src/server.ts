111b4c0dc7874ddc43074c405894dbc584c5bc6e
Agustí Moll <agusti@gmail.com>
2021-09-29 16:34:36 +0200

    logger

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express from 'express'
 import { tasksRouter } from './tasks/tasks.router'
 import { campaignsRouter } from './campaigns/campaigns.router'
 import Helmet from 'helmet'
+import morganMiddleware from './lib/morganMiddleware'
 
 const app = express()
 
 app.use(Helmet())
+app.use(morganMiddleware)
 app.use(express.json())
 
 app.get('/', (req, res) => {
